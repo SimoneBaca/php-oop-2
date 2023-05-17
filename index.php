2978840dcdabb10ed511693e1ec956ec9f1a093c
Simonebaca <simobaco99@gmail.com>
2023-05-17 17:15:31 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,6 @@
+<!--Immaginiamo le classi per modellizzare un personal computer.
+Un computer desktop é un computer.
+Un computer portatile é un computer.
+Creiamo la classe computer come parent class ed estendiamola per le classi desktop e laptop.
+Creiamo un set di dati in forma di array di oggetti e stampiamoli a schermo in una card usando bootstrap.
+Nella card, indichiamo anche che tipo di prodotto stiamo visualizzando (desktop, laptop) creando un apposito metodo poliforfo in ciascuna sottoclasse.-->
